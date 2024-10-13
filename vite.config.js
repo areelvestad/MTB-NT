3d864dbb2d73665aedbec64b29996359058f237a
areelvestad <areelvestad@yahoo.com>
2024-10-14 01:06:31 +0200

    new test

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite';
 
-// Vite configuration for TypeScript projects without React
 export default defineConfig({
-  build: {
-    outDir: 'dist', // or another directory where you want the output to go
-  },
-  server: {
-    open: true, // Opens the app in the browser automatically
-  },
-  resolve: {
-    alias: {
-      // Add any aliases you need here, like '@' to point to 'src'
-    },
-  },
+  base: '/BikePageVite/', // This ensures correct paths for GitHub Pages
+  // other Vite configuration
 });
